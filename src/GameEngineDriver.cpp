6c4a229906d2290f5e0d889463cf0ed29a3a187c
max <max@aka-thinkpad>
2024-10-02 17:02:15 -0400

    changed player names, yall are boring

--- src/GameEngineDriver.cpp
+++ src/GameEngineDriver.cpp
@@ -3,7 +3,7 @@
 void testGameEngine(const std::string& map_fileName)
 {
     std::cout << "==================TEST GAME ENGINE==================" << std::endl;
-    std::string command_tests = "load_map\nvalidate_map\nadd_player\n3\nnvidia_drivers_enjoyer\nxXxMikuLuvxXx\nnullPointerDereferencer41\nassign_countries\nissue_order\n4\n5\n6\nend_issue_orders\nexec_orders\nend_exec_orders\nwin\nplay\nquit\n";
+    std::string command_tests = "load_map\nvalidate_map\nadd_player\n3\nPlayer1\nPlayer2\nPlayer3\nassign_countries\nissue_order\n4\n5\n6\nend_issue_orders\nexec_orders\nend_exec_orders\nwin\nplay\nquit\n";
 
     std::cout << ">>[DEBUG]: ==== Running tests for Game Engine class ====" << std::endl;
     GameEngine* gameEngineTest = new GameEngine(map_fileName);
